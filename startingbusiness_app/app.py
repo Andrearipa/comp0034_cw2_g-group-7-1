9e721e9588dfc554f469b23f062b49a86eb32eb2
Andrearipa <91899514+Andrearipa@users.noreply.github.com>
2022-02-22 16:46:56 +0000

    set up (incomplete)

--- startingbusiness_app/app.py
+++ startingbusiness_app/app.py
@@ -1,11 +1,7 @@
 from startingbusiness_app import create_app, config
+from startingbusiness_app.config import DevelopmentConfig
 
 app = create_app(config.DevelopmentConfig)
-
-'''app.config['TESTING'] = True
-app.config.from_object('yourapplication.default_settings')
-app.config.from_envvar('YOURAPPLICATION_SETTINGS')'''
-
 
 @app.route('/')
 def hello_world():  # put application's code here
--- startingbusiness_app/config.py
+++ startingbusiness_app/config.py
@@ -1,14 +1,32 @@
+"""Flask config class."""
+from pathlib import Path
+
+
 class Config(object):
-    pass
+    """
+    Key generated using:
+        import secrets
+        print(secrets.token_urlsafe(16))
+    """
+    DEBUG = False
+    SECRET_KEY = 'n_MImT106bAHxxAn2eogow'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DATA_PATH = Path('Data Set')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + str(DATA_PATH.parent.joinpath('choropleth_app.sqlite'))
 
 
 class ProductionConfig(Config):
+    ENV = 'production'
     pass
 
 
 class DevelopmentConfig(Config):
-    pass
+    ENV = 'development'
+    DEBUG = True
+    SQLALCHEMY_ECHO = True
 
 
 class TestingConfig(Config):
-    pass
+    ENV = 'testing'
+    TESTING = True
+    SQLALCHEMY_ECHO = True
