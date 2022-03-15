42fd41bf87308daed26545687aa4884c873cd62b
Andrearipa <91899514+Andrearipa@users.noreply.github.com>
2022-03-15 12:45:45 +0000

    testing conftest database issue

--- tests/conftest.py
+++ tests/conftest.py
@@ -28,20 +28,22 @@ def db(app):
                       password='Hello456', account_type='Professional')
         user_3 = User(first_name='Danielle', last_name='Konig', email='danielle_konig@123.co.uk', password='Canada789',
                       account_type='Entrepreneur')
-        database.session.add(user_1)
+        # database.session.add(user_1)
         database.session.add(user_2)
         database.session.add(user_3)
-        database.session.commit()
+        # database.session.commit()
 
-
+        user_1.posts.append(Blog(title='Test Post 1', content='Content for test post 1 by user 1'))
+        """
         post_1 = Blog(title='Test Post 1', content='Content for test post 1 by user 1', author='kate.vanelli@gmail.com')
         post_2 = Blog(title='Test Post 2', content='Content for test post 2 by user 1')
         post_3 = Blog(title='Test Post 3', content='Hello pizza is good')
-        database.session.add(post_1)
-        database.session.add(post_2)
-        database.session.add(post_3)
+        """
+        database.session.add(user_1)
+        # database.session.add(post_2)
+        # database.session.add(post_3)
 
         database.session.commit()
 
     yield database
-    database.drop_all()
+    # database.drop_all()
